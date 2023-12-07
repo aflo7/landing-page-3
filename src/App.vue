c1c3016bf894a952930955bae8cad304cb6e7877
Andres Flores <andresflo883@gmail.com>
2023-12-07 07:49:09 -0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,105 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue';
+import Nav from './components/Nav.vue';
+</script>
+
+<template>
+  <div>
+    <div
+      style="
+        display: flex;
+        justify-content: center;
+        background-color: rgb(24, 24, 24);
+      "
+    >
+      <Nav />
+    </div>
+    <div>
+      <img
+        src="https://www1.lifestylecommunities.com/sliders/germantownlc-0419-9-large.jpg"
+        height="600px"
+      />
+    </div>
+
+    <div
+      style="
+        display: flex;
+        justify-content: center;
+        background-color: rgb(24, 24, 24);
+        color: white;
+        padding: 40px;
+      "
+    >
+      <div style="width: 1200px; display: flex; gap: 50px">
+        <p style="font-size: 3rem; width: 1600px">
+          Love where you live, and feel like you belong.
+        </p>
+        <div>
+          <p style="font-size: 2rem">Made By Andres Flores</p>
+          <p style="font-size: 1.2rem">
+            For us, from day one, it's always been about building connections.
+            We're not just building apartments, restaurants, pools and gyms;
+            we're building places for people to engage, to connect, to be
+            themselves. We're building places to call home.
+          </p>
+        </div>
+      </div>
+    </div>
+
+    <div style="display: flex; justify-content: center">
+      <div class="grid-container">
+        <div>Andres Flores</div>
+        <div>Andres Flores</div>
+        <div>Andres Flores</div>
+        <div>Andres Flores</div>
+        <div>Andres Flores</div>
+        <div>Andres Flores</div>
+      </div>
+    </div>
+
+    <div
+      style="
+        display: flex;
+        justify-content: center;
+        color: white;
+        background-color: rgb(26, 26, 26);
+        padding: 40px;
+      "
+    >
+      <div
+        style="width: 1200px; display: flex; flex-direction: column; gap: 40px"
+      >
+        <div style="display: flex; gap: 100px">
+          <div class="left-footer">
+            <div>Locations</div>
+            <div>FAQ</div>
+            <div>The Goat</div>
+            <div>Collective House Realty</div>
+            <div>Code</div>
+          </div>
+          <div class="left-footer">
+            <div>Contact</div>
+            <div>Terms + Conditions</div>
+            <div>Privacy</div>
+            <div>Made By Andres Flores</div>
+          </div>
+        </div>
+        <div style="display: flex; gap: 40px; align-items: center">
+          <div>Sign up for updates, news, and more.</div>
+          <div style="display: flex; gap: 10px">
+            <input
+              type="text"
+              placeholder="email address"
+              style="padding: 10px; border: none"
+            />
+            <button type="submit" style="padding: 10px" id="submitBtn">
+              Submit
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
---
+++ src/components/Nav.vue
@@ -0,0 +1,50 @@
+<script setup>
+import { ref } from 'vue';
+// import { IoMdArrowDropdown } from 'react-icons/io';
+import { RiArrowDownSFill } from 'oh-vue-icons/icons';
+import { MdMailoutlineSharp } from 'oh-vue-icons/icons';
+defineProps({
+  msg: String
+});
+
+const count = ref(0);
+</script>
+
+<template>
+  <nav
+    style="
+      display: flex;
+      justify-content: space-between;
+      background-color: rgb(24, 24, 24);
+      color: white;
+      padding: 20px;
+      width: 1200px;
+    "
+  >
+    <img src="../assets/lifestyle-communities.jpeg" height="60px" />
+    <div
+      class="nav-center"
+      style="display: flex; gap: 20px; align-items: flex-end"
+    >
+      <div>
+        <div>LOCATIONS</div>
+      </div>
+      <div>
+        <div>LC STORY</div>
+      </div>
+      <div>
+        <div>BLOG</div>
+      </div>
+      <div>NEWS + EVENTS</div>
+      <div>CAREERS</div>
+      <div>CONTACT</div>
+    </div>
+    <div class="nav-right" style="display: flex; gap: 20px">
+      <div>Email Us</div>
+      <div>614.918.2000</div>
+      <div>Login</div>
+    </div>
+  </nav>
+</template>
+
+<style scoped></style>
